adacfabfd0c883c6324ae395dfeede8281d9f738
meet67677 <meet79907@gmail.com>
2025-04-29 14:21:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Paste all the JavaScript content from the <script> tag here
+
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Continue with all the JavaScript from the original code
